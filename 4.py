56d076f38f9ab936262255c2018d59c66a15d0f7
Vicente Rivera <vicenterivera225@gmail.com>
2024-12-02 21:34:12 -0300

    renames y act 4

---
+++ 4.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from grafo import G
+
+# Ejercicio 4
+a = 0.6
+
+# en data viene un diccionario con los pesos
+for u, v, data in G.edges(data=True):
+    data["peso"] = a * data["costo"] + (1 - a) * data["tiempo"]
+
+
+plt.figure(figsize=(12, 8))
+layout = nx.spring_layout(G, seed=1)
+nx.draw(
+    G, layout, with_labels=True, node_size=800, node_color="lightblue"
+)
+
+edge_labels = {(u, v): f"{d['peso']:.2f}" for u, v, d in G.edges(data=True)}
+nx.draw_networkx_edge_labels(G, layout, edge_labels=edge_labels)
+plt.show()
--- grafo.py
+++ grafo.py
@@ -1,37 +1,37 @@
 import networkx as nx
 
-COSTO_GALON = 4.32 # costo del galón EEUU
+COSTO_GALON = 4.32  # costo del galón EEUU
 EFICIENCIA = 5.7  # millas por galón
 
 
 G = nx.Graph()
 
 ciudades = [
-    "Los Angeles", 
-    "San Francisco", 
-    "San Diego", 
-    "Sacramento", 
-    "Reno", 
-    "Las Vegas", 
-    "Carson City", 
-    "Fresno", 
-    "Bakersfield", 
-    "Oakland", 
-    "San Jose", 
-    "Santa Barbara", 
-    "Stockton", 
-    "Modesto", 
-    "Chico", 
+    "Los Angeles",
+    "San Francisco",
+    "San Diego",
+    "Sacramento",
+    "Reno",
+    "Las Vegas",
+    "Carson City",
+    "Fresno",
+    "Bakersfield",
+    "Oakland",
+    "San Jose",
+    "Santa Barbara",
+    "Stockton",
+    "Modesto",
+    "Chico",
     "Palm Springs",
-    "Elko",  
-    "Henderson", 
+    "Elko",
+    "Henderson",
     "Lake Tahoe",
     "Riverside"
 ]
 G.add_nodes_from(ciudades)
 
 rutas = [
-    # Los angeles
+    # Los Angeles
     ("Los Angeles", "San Diego", {"costo": 90, "tiempo": 2}),
     ("Los Angeles", "Las Vegas", {"costo": 203, "tiempo": 4}),
     ("Los Angeles", "Santa Barbara", {"costo": 30, "tiempo": 0.6}),
@@ -40,15 +40,15 @@ rutas = [
 
     # Las Vegas
     ("Las Vegas", "Henderson", {"costo": 13, "tiempo": 0.4}),
-    
-    # san diego
+
+    # San Diego
     ("San Diego", "Riverside", {"costo": 73, "tiempo": 1.5}),
-    
+
     # Bakersfield
     ("Bakersfield", "Riverside", {"costo": 125, "tiempo": 2.5}),
     ("Bakersfield", "Fresno", {"costo": 82, "tiempo": 1.5}),
     ("Bakersfield", "Stockton", {"costo": 177, "tiempo": 3.5}),
-    
+
     # Stockton
     ("Stockton", "Modesto", {"costo": 21, "tiempo": 0.5}),
     ("Stockton", "San Jose", {"costo": 60, "tiempo": 1}),
@@ -59,25 +59,26 @@ rutas = [
     ("San Francisco", "Sacramento", {"costo": 64, "tiempo": 1.5}),
     ("San Francisco", "San Jose", {"costo": 36, "tiempo": 1}),
 
-    # san jose
+    # San Jose
     ("San Jose", "Oakland", {"costo": 30, "tiempo": 0.7}),
 
-    # reno
+    # Reno
     ("Reno", "Lake Tahoe", {"costo": 28, "tiempo": 0.8}),
     ("Reno", "Carson City", {"costo": 23, "tiempo": 0.5}),
     ("Reno", "Sacramento", {"costo": 99, "tiempo": 2}),
 
-    #Carson City
+    # Carson City
     ("Carson City", "Elko", {"costo": 239, "tiempo": 4.5}),
-    
-    #sacramento
+
+    # Sacramento
     ("Sacramento", "Chico", {"costo": 68, "tiempo": 1.5}),
 
-    #Oakland
+    # Oakland
     ("Oakland", "Palm Springs", {"costo": 362, "tiempo": 7.5}),
 
 ]
 
 G.add_edges_from(rutas)
-print(nx.shortest_path(G, "San Diego", "Elko", weight="costo"))
-print(nx.is_connected(G))
+# print(nx.shortest_path(G, "San Diego", "Elko", weight="costo"))
+# print(nx.is_connected(G))
+
